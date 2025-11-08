5680a2af1bef4e18eb61ea52a23b3a708cf38230
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-08 17:14:31 -0500

    Add product model

--- db/models.py
+++ db/models.py
@@ -6,11 +6,10 @@ except Exception:
     print('Exception: Django Not Found, please install it with "pip install django".')
     sys.exit()
 
-
-# Sample User model
-class User(models.Model):
-    name = models.CharField(max_length=50, default="Dan")
+class Product(models.Model):
+    upc = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return self.name
-
+        return f"{self.upc} | {self.name} | ${self.price}"
